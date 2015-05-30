95b356bb39f72ac0e9867d94317288f7d2b24bea
Shaw Innes <shaw@immortal.net.au>
2015-05-31 09:24:40 +1000

    Adding New Role for Base Server

---
+++ vagrant/chef-repo/cookbooks/role-baseserver/metadata.rb
@@ -0,0 +1,8 @@
+name             'role-baseserver'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures role-baseserver'
+long_description 'Installs/Configures role-baseserver'
+version          '0.1.0'
+
