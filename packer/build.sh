479b748940a0fc6aff548b72905547cc245917ce
Shaw Innes <shaw@immortal.net.au>
2015-08-02 15:48:53 +1000

    Various Updates

--- packer/build.sh
+++ packer/build.sh
@@ -6,23 +6,5 @@ VMNAME="server$EDITION_$VMVER"
 
 PATH=$PATH:"/Applications/VMware Fusion.app/Contents/Library/VMware OVF Tool"
 
-TEMPLATE=`knife vsphere template list --folder "templates" --config ./knife.rb`
-
-if echo $TEMPLATE | grep -q "$VMNAME"; then
-  echo Template "$VMNAME" Already Exists
-
-  exit 1
-fi
-
-VM=`knife vsphere vm list --folder "templates"`
-
-if echo $VM | grep -q "$VMNAME"; then
-  echo Virtual Machine "$VMNAME" Already Exists
-
-  knife vsphere vm markastemplate "$VMNAME" --folder "templates" --config ./knife.rb
-
-  exit 0
-fi
-
-packer build -var "vmname=$VMNAME" -var "edition=$EDITION" server2012r2_vsphere.json
+packer build -var "vmname=$VMNAME" -var "edition=$EDITION" server2012r2.json
 
