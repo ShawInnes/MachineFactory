8ecfac1a890eab63e1c3b597d5fd895279996cf9
Shaw Innes <shaw@immortal.net.au>
2015-08-12 14:13:56 +1000

    Added -Force to avoid stupid warnings

--- windows/scripts/windowsupdate-disable.ps1
+++ windows/scripts/windowsupdate-disable.ps1
@@ -1,4 +1,4 @@
 
-(New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name WindowsUpdate) | Out-Null
-(New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name AU) | Out-Null
-(New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name NoAutoUpdate -Value 1) | Out-Null
+(New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name WindowsUpdate -Force) | Out-Null
+(New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name AU -Force) | Out-Null
+(New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name NoAutoUpdate -Value 1 -Force) | Out-Null
