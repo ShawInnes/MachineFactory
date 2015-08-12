d04164f444a3afe68084f20cfd003314014e0d1b
Shaw Innes <shaw@immortal.net.au>
2015-08-12 18:46:29 +1000

    Working on Agent

--- windows/chef-repo/cookbooks/role-teamcityagent/recipes/default.rb
+++ windows/chef-repo/cookbooks/role-teamcityagent/recipes/default.rb
@@ -10,6 +10,8 @@
 # include_recipe 'nodejs-windows::default'
 
 include_recipe "chef-teamcity::windows_agent"
+include_recipe 'nodejs-windows::default'
+include_recipe 'visualstudio::default'
 
 windows_firewall_rule 'TeamCity Agent' do
   localport '9090'
@@ -18,4 +20,16 @@ windows_firewall_rule 'TeamCity Agent' do
   firewall_action :allow
 end
 
-include_recipe 'nodejs-windows::default'
+directory node['teamcity']['agent']['work_dir'] do
+  rights :full_control, 'Everyone', :applies_to_children => true
+end
+
+powershell_script "Share Work Folder" do
+  guard_interpreter :powershell_script
+  code <<-EOH
+    New-SmbShare -Name "Work" -Path "#{node['teamcity']['agent']['work_dir']}"
+  EOH
+  not_if <<-EOH
+    (Get-SmbShare -Name "Work").Name -eq "Work"
+  EOH
+end
