63521582705a830283185cc4384977f9092a9b04
Shaw Innes <shaw@immortal.net.au>
2015-08-13 21:09:14 +1000

    Add SQL Server Role

---
+++ windows/chef-repo/cookbooks/role-sqlserver/README.md
@@ -0,0 +1,4 @@
+# role-sqlserver
+
+TODO: Enter the cookbook description here.
+
---
+++ windows/chef-repo/cookbooks/role-sqlserver/metadata.rb
@@ -0,0 +1,7 @@
+name 'role-sqlserver'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures role-sqlserver'
+long_description 'Installs/Configures role-sqlserver'
+version '0.1.0'
